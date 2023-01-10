dc0b3d1c7eab61bb7adde77928f87b337ea8dc1f
william <53148007+willFrazao@users.noreply.github.com>
2023-01-10 10:58:52 -0300

    pasta src
    adicionando a pasta src

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Module Routing
+import { RoutingModule } from './routing.modules';
+
+// Pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+    declarations: [
+        HomeComponent,
+        DetailsComponent
+    ],
+    imports: [
+        CommonModule,
+        RoutingModule,
+        SharedModule,
+    ]
+})
+export class PagesModule { }
